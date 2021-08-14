3e0da71f94b443f326c54bda85795c0db93d7063
Garvit Chouhan <68856476+Garvit9000c@users.noreply.github.com>
2021-08-14 23:49:00 +0530

    Update drive.py

--- drive.py
+++ drive.py
@@ -24,8 +24,3 @@ def save_response_content(response, destination):
         for chunk in response.iter_content(CHUNK_SIZE):
             if chunk: # filter out keep-alive new chunks
                 f.write(chunk)
-                
-
-file_id = 'https://drive.google.com/file/d/1WJTpAUwjc1Tvtw5Y4c2n1q3W8VV4Db1T/view?usp=sharing'
-destination = './uploads/alpha'
-download(file_id.split('/')[5] , destination)
